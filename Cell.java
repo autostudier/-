dbc43f55351c2549dd3fc3ce98470f9ed4bf3337
autostudier <32011482+autostudier@users.noreply.github.com>
2019-07-08 17:30:01 +0800

    Create Cell.java

---
+++ Cell.java
@@ -0,0 +1,22 @@
+package cn.guida.snake;
+/**
+ * 这是格子类——造食物和蛇
+ * @author Administrator
+ *
+ */
+public class Cell {
+	/*格子的宽*/
+	int width = 15;
+	/*格子的长*/
+	int height =15;
+	/*格子的x坐标*/
+	int x;
+	/*格子的y坐标*/
+	int y;
+
+}
+
+
+
+
+
